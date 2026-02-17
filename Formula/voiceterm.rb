22d1406110c265de6354ea64a73617c099388db7
Justin Guida <justin.guida@snhu.edu>
2026-02-17 07:49:15 -0500

    Update to v1.0.77

--- Formula/voiceterm.rb
+++ Formula/voiceterm.rb
@@ -1,9 +1,9 @@
 class Voiceterm < Formula
   desc "Voice-first HUD overlay for AI CLIs with local Whisper STT"
   homepage "https://github.com/jguida941/voiceterm"
-  url "https://github.com/jguida941/voiceterm/archive/refs/tags/v1.0.75.tar.gz"
-  version "1.0.75"
-  sha256 "659abcec6238f4439d0a942fc86126ac3c19d53acb9b1b65deeabc6fb59a98d0"
+  url "https://github.com/jguida941/voiceterm/archive/refs/tags/v1.0.77.tar.gz"
+  version "1.0.77"
+  sha256 "74707e76bec5e3cd50fe5f1011cd828fb637e648baf29dfeb73bada95efbc791"
 
   depends_on "rust" => :build
   depends_on "cmake" => :build
--- README.md
+++ README.md
@@ -32,4 +32,4 @@ brew upgrade voiceterm
 
 ## Version
 
-Current: v1.0.75
+Current: v1.0.77
